6c53c0799caa5027bd84ad87eea5554f015c39a1
Nick Jordan <nickjordan2002@gmail.com>
2025-01-05 09:40:23 +0100

    ECHOE-100 | Implementing assets & Setup LoadoutDisplay
    [REFACTOR] Turned overworld button in LoadoutDisplay into ImageButton and removed redundant class.

--- MonoZenith/Screen/LoadoutDisplay/LoadoutDisplay.cs
+++ MonoZenith/Screen/LoadoutDisplay/LoadoutDisplay.cs
@@ -1,14 +1,32 @@
-using MonoZenith.Components.LoadoutDisplay;
+using Microsoft.Xna.Framework;
+using MonoZenith.Components;
+using MonoZenith.Engine.Support;
+using static MonoZenith.Game;
 
 namespace MonoZenith.Screen.LoadoutDisplay;
 
 public class LoadoutDisplay
 {
-    private readonly BackToOverworldButton _backToOverworldButton = new();
-    
+    private readonly ImageButton _backToOverworldButton;
+
+    // ReSharper disable once ConvertConstructorToMemberInitializers
+    public LoadoutDisplay()
+    {
+        _backToOverworldButton = new ImageButton(
+            new Vector2(30 * AppSettings.Scaling.ScaleFactor, 30 * AppSettings.Scaling.ScaleFactor),
+            LoadImage("Images/LoadoutDisplay/Buttons/to-overworld.png"),
+            scale: 0.18f,
+            onClickAction: () =>
+            {
+                DataManager.GetInstance().EndPlayerTurnSound.CreateInstance().Play();
+                ShowLoadoutDisplay(false);
+            }
+        );
+    }
+        
     public void Update()
     {
-        _backToOverworldButton.Update();
+        _backToOverworldButton.Update(DeltaTime);
     }
 
     public void Draw()
